0ffd1c373c1ad9b04b722e447a3a34ba3b307ec1
Oleksandr Markov <ol.markov1999@gmail.com>
2022-03-19 13:32:58 +0200

    Work with text and fonts

--- main.lua
+++ main.lua
@@ -4,30 +4,40 @@
 
 display.setStatusBar(display.HiddenStatusBar)
 
---display.setDefault("fillColor", 34 / 255, 45 / 255, 56 / 255)
 display.setDefault("background", 119 / 255, 15 / 255, 64 / 255) -- rgb
+display.setDefault("fillColor", 0)
 --display.setDefault("background", 0) -- 0 = black, 0.5 = grey, 1 = white
 --display.setDefault("background", 0.5, 0) -- 1st is color, 2nd is transperency(прозрачность: 0 - absolute transperency)
  
- --w = display.contentWidth - 20
-
- --circle = display.newCircle(display.contentCenterX, 74, 60) -- y = topY - 74, radius = 60
- --circle:setFillColor(66 / 255, 135 / 255, 245 / 255)
- 
- --rectangle = display.newRoundedRect(display.contentCenterX, 110, w, 100, 15 ) -- y = topY - 110, height = 100, radius = 15
- --rectangle:setFillColor(66 / 255, 135 / 255, 245 / 255)
+w = display.contentWidth - 20
+weightMin = 40
+weightMax = 140
+weight = weightMin
  
 local weightGroup = display.newGroup()
  
-circle = display.newCircle(weightGroup, display.contentCenterX, 74, 60)
+circle = display.newCircle(weightGroup, display.contentCenterX, 54, 50) -- y = topY - 54, radius = 50
 circle:setFillColor(66 / 255, 135 / 255, 245 / 255)
  
-circle = display.newCircle(weightGroup, display.contentCenterX, 254, 60)
-circle:setFillColor(66 / 255, 135 / 255, 245 / 255)
-
+rectangle = display.newRoundedRect(weightGroup, display.contentCenterX, 110, w, 100, 10 ) -- y = topY - 110, height = 100, radius = 15
+rectangle:setFillColor(66 / 255, 135 / 255, 245 / 255) 
+ 
 --weightGroup:removeSelf()
 --display.remove(weightGroup) -- the same
 
-weightGroup.y = 100
-weightGroup.x = 50
--- previous X=Y=0
+-- by default X=Y=0
+--weightGroup.y = 100
+--weightGroup.x = 50
+
+display.newText(weightGroup, "Укажите вес в килограммах:", display.contentCenterX, 90, "SuezOne-Regular", 20) -- size = 20
+
+local myWeight = display.newText({
+	parent = weightGroup,
+	fontSize = 35,
+	text = weight,
+	x = display.contentCenterX,
+	y = 53,
+	font = "Obelix Pro", -- native.systemFont
+})
+
+myWeight: setFillColor(19 / 255, 3 / 255, 56 / 255)

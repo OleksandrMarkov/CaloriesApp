6c0e0dea30d597b906184986e9c947b64b299f44
Oleksandr Markov <ol.markov1999@gmail.com>
2022-03-19 12:04:10 +0200

    StatusBar hiding and background set

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
-local bg = display.newImage("bg.jpg")
-bg.x = display.contentCenterX
-bg.y = display.contentCenterY
+--local bg = display.newImage("bg.jpg")
+--bg.x = display.contentCenterX
+--bg.y = display.contentCenterY
 
-display.setDefault("background", 185 / 255, 245 / 255, 245 / 255) --default
+--display.setDefault("background", 185 / 255, 245 / 255, 245 / 255) --default
+
+display.setStatusBar(display.HiddenStatusBar)
+display.setDefault("fillColor", 34 / 255, 45 / 255, 56 / 255)
+--display.setDefault("background", 34 / 255, 45 / 255, 56 / 255) -- rgb
+
+display.setDefault("background", 0) -- 0 = black, 0.5 = grey, 1 = white
+
+display.setDefault("background", 0.5, 0) -- 1st is color, 2nd is transperency(прозрачность: 0 - absolute transperency)
+ 
